cf1f039792ed8e454b1944dc304d890d6483aab7
vivek nath tiwari <viveknathtiwari01@gmail.com>
2025-05-06 21:57:44 +0530

    initial commit

---
+++ data/testimonial.js
@@ -0,0 +1,28 @@
+export const testimonial = [
+  {
+    quote:
+      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+    author: "Vivek Nath Tiwari",
+    image: "/vivek_testi.jpg",
+    role: "Software Engineer",
+    company: "Tech Giant Co.",
+  },
+  {
+    quote:
+      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+    author: "Raviraj Kumar Tiwari",
+    image: "/vivek_testi_2.jpg",
+    role: "AI/ML Engineer",
+    company: "Global Corp",
+    
+  },
+  {
+    quote:
+      "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
+    author: "Anu Priya",
+    image: "/anu.jpg",
+    role: "Data Scientist",
+    company: "Data Analytics Inc.",
+    
+  },
+];
